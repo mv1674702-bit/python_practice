f5681316166fd51565b270b58418839963959b23
mv1674702-bit <mv1674702@gmail.com>
2026-03-04 21:07:45 +0530

    updating the previous code

--- webdev/appdev/models.py
+++ webdev/appdev/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-
-Windsurf: Refractor|Explain
 class Tableone(models.Model):
     name=models.CharField(max_length=100)
     mobile=models.CharField(max_length=10)
     email=models.CharField(max_length=100)
-
+    file=models.FileField(upload_to='media/', null=True, blank=True)
--- webdev/appdev/views.py
+++ webdev/appdev/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from appdev.models import Tableone
 
 # Create your views here.
 
 def home(request):
-    if request.method == 'GET':
-        name=request.GET.get('var_name')
-        mobile=request.GET.get('mobile')
-        email=request.GET.get('email')
+    if request.method == 'POST':
+        name=request.POST.get('var_name')
+        mobile=request.POST.get('mobile')
+        email=request.POST.get('email')
 
-        print(f"Name: {name}, Mobile: {mobile}, Email: {email}")
-    return render(request, 'home.html')
+        # print(f"Name: {name}, Mobile: {mobile}, Email: {email}")
+        Tableone.objects.create(name= name, mobile= mobile, email= email)
+        return redirect("/")
+    return render(request, "home.html")
